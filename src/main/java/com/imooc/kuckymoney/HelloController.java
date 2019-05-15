e11171889b236c13ee5f4a0dbb0fe678b1aa4e80
liqiankun <13671386471@163.com>
2019-05-16 01:25:08 +0800

    2小时Spring Boot课程

---
+++ src/main/java/com/imooc/kuckymoney/HelloController.java
@@ -0,0 +1,19 @@
+package com.imooc.kuckymoney;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.parsers.SAXParser;
+import java.math.BigDecimal;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private LimitConfig limitConfig;
+    @GetMapping("/hello")
+    public String say(){
+        return "学习Spring"+limitConfig.getDescription();
+    }
+}
---
+++ src/main/java/com/imooc/kuckymoney/KuckymoneyApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.kuckymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuckymoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuckymoneyApplication.class, args);
+    }
+
+}
